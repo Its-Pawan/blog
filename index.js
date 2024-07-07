a205b50fddce9762fe3d5428d98fb26fd5b5cf25
Pawan Joshi <business.pawanjoshi@gmail.com>
2024-07-07 23:12:41 +0530

    models is now in indes.js file

--- index.js
+++ index.js
@@ -7,9 +7,49 @@ import 'dotenv/config'
 import mongoose from 'mongoose';
 import session from 'express-session';
 
-// importing models
-import Auth from './models/auth.js';
-import Blog from './models/blog.js';
+// =====================================================
+// importing models 
+// import Auth from './models/auth.js';
+// import Blog from './models/blog.js';
+
+// models data
+// Blog ===========
+const blogSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    description: String,
+    image: {
+        data: Buffer,
+        contentType: String
+    },
+    publishDate: {
+        type: Date,
+        default: Date.now,
+        get: (date) => date.toISOString().split('T')[0] // Format the date as YYYY-MM-DD
+    }
+}, {
+    toJSON: { getters: true },
+    toObject: { getters: true }
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+
+// Auth======================
+const authSchema = new mongoose.Schema({
+    username: String,
+    password: String
+
+}, {
+    toJSON: { getters: true },
+    toObject: { getters: true }
+});
+
+const Auth = mongoose.model('auth', authSchema);
+
+
+// ============================================================
+
 
 // Set up multer for file uploads 
 const upload = multer({ storage: multer.memoryStorage() });
