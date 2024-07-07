99d54f3bcd7647891cb76e2ded20eec605921b32
Pawan Joshi <business.pawanjoshi@gmail.com>
2024-07-07 23:07:49 +0530

    update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import mongoose from 'mongoose';
 import session from 'express-session';
 
 // importing models
-import Auth from './models/auth.js'
-import Blog from './models/blog.js'
+import Auth from './models/auth.js';
+import Blog from './models/blog.js';
 
 // Set up multer for file uploads 
 const upload = multer({ storage: multer.memoryStorage() });
